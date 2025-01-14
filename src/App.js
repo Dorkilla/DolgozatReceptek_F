daf9c8bb3688b44a6688d1ccb336ab85ed5fa07c
Dorkilla <dorkilla58@gmail.com>
2025-01-15 00:44:40 +0100

    vegee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import TablazatReceptek from './components/TablazatReceptek';
+import Kategoria from './components/Kategoria';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Receptek</h1>
       </header>
+      <main>
+      <Kategoria/>
+      <TablazatReceptek/>
+      </main>
     </div>
-  );
+ );
 }
 
 export default App;
